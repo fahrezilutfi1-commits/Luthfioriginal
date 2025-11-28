4d22e09b98b56251479337645ba5dd5da68c0138
fahrezilutfi1-commits <fahrezilutfi1@gmail.com>
2025-11-28 09:58:21 +0800

    Create scrift.js

---
+++ js/scrift.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    document.getElementById('sidebar').classList.toggle('active');
+}
+function toggleMenu() {
+    document.getElementById('sidebar').classList.toggle('active');
+}
+
+// Menutup sidebar setelah memilih menu
+function closeMenu() {
+    document.getElementById('sidebar').classList.remove('active');
+}
